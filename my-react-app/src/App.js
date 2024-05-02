36ee505047783682e242ee6364695a4d66cd0d2c
PriyanshuKSharma <priyanshu17ks@gmail.com>
2024-05-02 18:45:58 +0530

    Modified files

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,7 +1,11 @@
 const App = () => {
   return (
     <div className="App">
-      
+      <section classNAme="search-sectoion"> 
+      <p> What do you want to know?
+        <button className="surprise">Surprise Me</button>
+      </p>
+      </section>
     </div>
   );
 }
